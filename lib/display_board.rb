e233a9d65f3b973234b9c291faf05bcad3ddb84b
Deja Monae Allison <dejaallison@gmail.com>
2020-06-12 01:12:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " "," "]
+  board = [" ", " ", " ", " ", "X", " ", " ", " "," "]
+  board = ["O", " ", " ", " ", " ", " ", " ", " "," "]
+
+
 end
-
-puts "a board with an x in the center position"
-board = [" ", " ", " ", " ", "X", " ", " ", " "," "]
-display_board
-
-puts "a board with a O in the top left"
-board = ["O", " ", " ", " ", " ", " ", " ", " "," "]
-display_board
